caafb4415715bbc1732672e7c799d2de73ed351e
Lorenzo Tinsley <lorenzotinsley@gmail.com>
2022-04-09 14:54:57 -0400

    Workshop 1

---
+++ components/ContactComponent.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { ScrollView, Text } from 'react-native';
+import { Card } from 'react-native-elements';
+
+
+class Contact extends Component {
+
+    static navigationOptions = {
+        title: 'Contact Us'
+    }
+
+    render() {
+        return (
+            <ScrollView>
+                <Card
+                    title={`Contact Information`}
+                    wrapperStyle={{ margin: 20 }}
+                >
+
+                    <Text>NuCamp Way</Text>
+                    <Text>Seattle, Wa, 98001</Text>
+                    <Text style={{ marginBottom: 10 }}>USA</Text>
+
+                    <Text>Phone: 1-206-555-1234</Text>
+                    <Text style={{ marginBottom: 10 }}>Email: campsites@nucamp.co</Text>
+                </Card>
+            </ScrollView>
+        );
+    }
+}
+
+export default Contact;
