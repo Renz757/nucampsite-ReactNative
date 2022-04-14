88c6e7ae8f9254968358e545f1665905eb4ba5e9
Lorenzo Tinsley <lorenzotinsley@gmail.com>
2022-04-14 13:23:04 -0400

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { ScrollView, FlatList } from 'react-native';
 import { Card, Text, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -20,7 +21,7 @@ const Mission = () => {
 }
 
 class About extends Component {
-    
+
     static navigationOptions = {
         title: 'About Us'
     }
@@ -28,21 +29,45 @@ class About extends Component {
 
 
     render() {
-
         const renderPartner = ({ item }) => {
             return (
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{ source: {uri: baseUrl + item.image}}}
+                    leftAvatar={{ source: { uri: baseUrl + item.image } }}
                 />
             );
-        }
+        };
 
+        if (this.props.partners.isLoading) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title="Community Partners">
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            )
+        }
+        if (this.props.partners.errMess) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card
+                        title="Community Partners">
+                        <Text>
+                            {this.props.partners.errMess}
+                        </Text>
+                    </Card>
+                </ScrollView>
+            )
+        }
         return (
             <ScrollView>
                 <Mission />
-                <Card title="Community Partners">
+                <Card
+                    title="Community Partners">
                     <FlatList
                         data={this.props.partners.partners}
                         renderItem={renderPartner}
